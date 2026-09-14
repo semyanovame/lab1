151847c0d6f510a697450da86fcbe4e2e4a1b44f
Семьянова Маргарита Евгеньевна <SemyanovaME@dom201.local>
2026-09-14 12:08:11 +0700

    start

--- main.py
+++ main.py
@@ -3,3 +3,22 @@ import re
 import sys
 import hashlib
 import logging
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+# Шаблон строки лога (аналог template в Serilog)
+# Содержит: время, уровень (до 7 символов для выравнивания), имя логгера и сообщение
+log_format = "%(asctime)s | [%(levelname)-7s] | %(message)s"
+date_format = "%Y-%m-%d %H:%M:%S"
+
+# Базовая настройка корневого логгера
+logging.basicConfig(
+    level=logging.DEBUG, # Минимальный уровень логирования (аналог MinimumLevel.Debug)
+    format=log_format,
+    datefmt=date_format,
+    handlers=[
+        logging.StreamHandler(sys.stdout),          # Настройка логирования в консоль
+        logging.FileHandler("logs/file_txt.log", encoding="utf-8") # Настройка логирования в файл
+    ]
+)
+logging.info("Логгер успешно сконфигурирован")
+logging.info("Приложение запущено")
